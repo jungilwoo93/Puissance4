1e86e8068953ffa4486dd398f7808901b18da652
Andy Chabalier <Andy@chabalier.com>
2018-05-05 23:11:12 +0200

    retablishment of launch application. If no argument is given, default mode is GUI application.

--- Pan_Chabalier_Puissance4/src/fr/uha/ensisa/puissance4/Puissance4.java
+++ Pan_Chabalier_Puissance4/src/fr/uha/ensisa/puissance4/Puissance4.java
@@ -5,17 +5,20 @@ import fr.uha.ensisa.puissance4.ui.GUI;
 
 public abstract class Puissance4 {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
+		int mode;
+		if (args[0] == null) {
+			mode = 2;
+		} else {
+			mode = Integer.parseInt(args[0]);
+		}
 
-//		int mode = Integer.parseInt(args[0]);
-		int mode = 2;
-		//Indique la bonne interface et la lance dans un thread différent
-		switch(mode)
-		{
+		// Indique la bonne interface et la lance dans un thread différent
+		switch (mode) {
 		case 2:
 			GUI.main(args);
 			break;
-		default :
+		default:
 			Console console = new Console();
 			console.start();
 			break;
